4f27d7f458b6c548c9d66e3062afd137b50bacc3
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2024-06-11 23:56:50 -0300

    feat: adicionando id gerado para usuarios

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,5 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Usuario
 {
+    [Key, Column(Order = 0)]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
